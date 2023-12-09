64edc7c1776d8066e4f4e569678e7018a938b0bb
MDodd <mick.dodd@gmail.com>
2023-12-09 10:25:07 +0000

    day 9

---
+++ day_9/part1.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Parses the contents of a file into an array of lines.
+ *
+ * @param string $filePath The path to the file.
+ * @return array The array of lines in the file.
+ *
+ * @throws InvalidArgumentException if the $filePath parameter is not a string.
+ */
+function parseInput(string $filePath): array
+{
+    return file($filePath, FILE_IGNORE_NEW_LINES);
+}
+
+/**
+ * Extrapolates the next values in a series based on historical values.
+ *
+ * @param array $lines The array of historical values.
+ *         Each line is a string of space-separated integers.
+ *         Example: ['1 2 3', '4 5 6'].
+ *
+ * @return int The sum of the extrapolated next values in the series.
+ *
+ * @throws InvalidArgumentException if the $lines parameter is not an array.
+ */
+function extrapolate(array $lines): int
+{
+    $histories = array_map(function ($line) {
+        return array_map('intval', explode(' ', $line));
+    }, $lines);
+
+    $nextValues = [];
+
+    foreach ($histories as $history) {
+        $sequence = $history;
+        $lastValues = [];
+
+        while (count($sequence) > 1) {
+            $newSequence = [];
+            for ($i = 0; $i < count($sequence) - 1; $i++) {
+                $newSequence[] = $sequence[$i + 1] - $sequence[$i];
+            }
+            $sequence = $newSequence;
+
+            if (count($sequence) > 0) {
+                array_unshift($lastValues, end($sequence));
+            }
+        }
+
+        $nextValue = array_sum($lastValues) + end($history);
+        $nextValues[] = $nextValue;
+    }
+
+    return array_sum($nextValues);
+}
+
+$filePath = 'input.txt';
+$lines = parseInput($filePath);
+
+$part1 = extrapolate($lines);
+echo "Sum of these extrapolated values: " . $part1 . "\n";
---
+++ day_9/part2.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Parses the input file and returns an array of strings.
+ *
+ * The method reads the content of the file specified by the $filePath parameter,
+ * ignoring the new line characters. It then returns an array, where each element
+ * represents a line from the file.
+ *
+ * @param string $filePath The path to the input file.
+ * @return array An array of strings, where each string represents a line from the file.
+ */
+function parseInput(string $filePath): array
+{
+    return file($filePath, FILE_IGNORE_NEW_LINES);
+}
+
+/**
+ * Extrapolates the next values in a sequence based on the given lines.
+ *
+ * Each line in the $lines array should contain integers separated by spaces, representing a sequence of values.
+ * The method calculates the next value in each sequence by finding the differences between consecutive values,
+ * summing the last values obtained, and adding it to the last value in the sequence.
+ * The method returns the sum of all the next values calculated.
+ *
+ * @param array $lines An array of strings, where each string represents a line of integers separated by spaces.
+ * @return int The sum of all the next values calculated in the sequences.
+ */
+function extrapolate(array $lines): int
+{
+    $histories = array_map(function ($line) {
+        return array_map('intval', explode(' ', $line));
+    }, $lines);
+
+    $histories = array_map('array_reverse', $histories);
+
+    $nextValues = [];
+
+    foreach ($histories as $history) {
+        $sequence = $history;
+        $lastValues = [];
+
+        while (count($sequence) > 1) {
+            $newSequence = [];
+            for ($i = 0; $i < count($sequence) - 1; $i++) {
+                $newSequence[] = $sequence[$i + 1] - $sequence[$i];
+            }
+            $sequence = $newSequence;
+
+            if (count($sequence) > 0) {
+                array_unshift($lastValues, end($sequence));
+            }
+        }
+
+        $nextValue = array_sum($lastValues) + end($history);
+        $nextValues[] = $nextValue;
+    }
+
+    return array_sum($nextValues);
+}
+
+$filePath = 'input.txt';
+$lines = parseInput($filePath);
+
+$part1 = extrapolate($lines);
+echo "Sum of these extrapolated values: " . $part1 . "\n";
+
